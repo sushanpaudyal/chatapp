a8523745248db6764c0a3a245c5698f313cb4a77
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-06-12 14:55:51 +0545

    Check this chat component

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,24 @@
+<template> 
+    <div class="container"> 
+        <div class="row justify-content-center"> 
+            <div class="col-md-3"> 
+                <div class="card card-default"> 
+                    <div class="card-header">Private Chat App</div> 
+                    <ul class="list-group"> 
+                        <li class="list-group-item">Hello</li> 
+                    </ul> 
+                </div> 
+            </div> 
+            <div class="col-md-9"> 
+            </div> 
+        </div> 
+    </div> 
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
